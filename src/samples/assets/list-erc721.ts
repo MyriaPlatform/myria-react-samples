52ede7ba774d76b37dad4ba84ea7c36f0bf813ba
markofdao <dmitrydaopro@gmail.com>
2022-09-07 18:35:41 +0700

    add list erc721

---
+++ src/samples/assets/list-erc721.ts
@@ -0,0 +1,53 @@
+import { CreateOrderEntity, DeveloperAccountManager, ModuleFactory, MyriaClient, SignableOrderInput, TokenType, WithdrawalModule, WithdrawNftOffChainParams } from "myria-core-sdk";
+
+export async function listErc721(client: MyriaClient, account: string, asset: any) {
+  const moduleFactory = new ModuleFactory(client);
+  const orderModule = moduleFactory.getOrderManager();
+  const devAccountManager: DeveloperAccountManager = new DeveloperAccountManager(client);
+  const starkKey = (await devAccountManager.getUserByWalletAddress(account)).starkKey;
+
+  if (!account) return;
+  const payload: SignableOrderInput = {
+    orderType: 'SELL',
+    ethAddress: account,
+    starkKey: starkKey,
+    tokenSell: {
+      type: TokenType.MINTABLE_ERC721,
+      data: {
+        tokenId: asset?.tokenId,
+        tokenAddress: asset?.tokenAddress
+      }
+    },
+    amountSell: "1",
+    tokenBuy: {
+      type: TokenType.ETH,
+      data: {
+        quantum: "1"
+      }
+    },
+    amountBuy: "0.001",
+    includeFees: false
+  };
+  
+  const signature = await orderModule?.signableOrder(payload);
+  if (signature) {
+    const paramCreateOrder: CreateOrderEntity = {
+      assetRefId: parseInt(asset.id, 10),
+      orderType: 'SELL',
+      fees: [{}],
+      includeFees: false,
+      amountSell: signature.amountSell,
+      amountBuy: signature.amountBuy,
+      sellerStarkKey: starkKey,
+      vaultIdSell: signature.vaultIdSell,
+      vaultIdBuy: signature.vaultIdBuy,
+      sellerAddress: account,
+      assetIdBuy: signature.assetIdBuy,
+      assetIdSell: signature.assetIdSell
+    };
+    console.log(paramCreateOrder);
+
+    const res = await orderModule?.createOrder(paramCreateOrder);
+    console.log(res);
+  }
+}
--- src/views/Assets.tsx
+++ src/views/Assets.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useState } from "react";
 import { getAssetsByStarkKey } from "../samples/assets/assets-by-stark-key";
+import { listErc721 } from '../samples/assets/list-erc721';
 import { withdrawErc721 } from "../samples/assets/withdraw-erc721";
 import { getMyriaClient } from '../samples/common/myria-client';
 
@@ -10,7 +11,7 @@ type Props = {
 }
 
 const Assets = ({ isConnected, account }: Props) => {
-	const [nfts, setNfts] = useState(null);
+	const [assets, setAssets] = useState(null);
 	const [isLoading, setIsLoading] = useState(false);
 	const [isLoaded, setIsLoaded] = useState(false);
 	const [err, setErr] = useState('');
@@ -21,7 +22,7 @@ const Assets = ({ isConnected, account }: Props) => {
 			try {
 				const client = await getMyriaClient(isConnected);
 				const result = await getAssetsByStarkKey(client, account);
-				setNfts(result);
+				setAssets(result);
 			} catch (err: any) {
 				setErr(err.message);
 			} finally {
@@ -33,10 +34,16 @@ const Assets = ({ isConnected, account }: Props) => {
 		getNfts();
 	}, []);
 
-	const withdrawNft = async (param: any) => {
+	const withdrawNft = async (asset: any) => {
 		const client = await getMyriaClient(isConnected);
-		const withdrawalResult = await withdrawErc721(client, param, account);
+		const withdrawalResult = await withdrawErc721(client, asset, account);
 		console.log(withdrawalResult);
+	}
+
+	const listNft = async (asset: any) => {
+		const client = await getMyriaClient(isConnected);
+		const result = await listErc721(client, account, asset);
+		console.log(result);
 	}
 
 	return (
@@ -46,24 +53,24 @@ const Assets = ({ isConnected, account }: Props) => {
 			{isLoading && <p>Loading assets...</p>}
 
 			<div className="row align-items-start text-center mt-3">
-				{(Array.isArray(nfts) && isLoaded)
-					? nfts.map((nft: any) => (
-						<div className="col mb-3" key={nft.id}>
-							<div className="card" key={nft.id} style={{ width: "14rem" }}>
-								<img src={nft.imageUrl ? nft.imageUrl : "/null.png"} alt={nft.name} className="card-img-top img-thumbnail" />
+				{(Array.isArray(assets) && isLoaded)
+					? assets.map((asset: any) => (
+						<div className="col mb-3" key={asset.id}>
+							<div className="card" key={asset.id} style={{ width: "14rem" }}>
+								<img src={asset.imageUrl ? asset.imageUrl : "/null.png"} alt={asset.name} className="card-img-top img-thumbnail" />
 								<div className="card-body">
-									<h5 className="card-title">{nft.name}</h5>
-									<p className="card-text">{nft.description}</p>
-									<a className="card-link" onClick={() => withdrawNft(nft)}>Withdraw NFT</a>
-									<a className="card-link">List NFT</a>
+									<h5 className="card-title">{asset.name}</h5>
+									<p className="card-text">{asset.description}</p>
+									<a className="card-link" onClick={() => withdrawNft(asset)}>Withdraw NFT</a>
+									<a className="card-link" onClick={() => listNft(asset)}>List NFT</a>
 								</div>
 								<div className="card-footer">
-									<small className="text-muted">#{nft.id} | {nft.publicId}</small>
+									<small className="text-muted">#{asset.id} | {asset.publicId}</small>
 								</div>
 							</div>
 						</div>
 					))
-					: (isLoaded && !(Array.isArray(nfts)) && <p>No assets available</p>)
+					: (isLoaded && !(Array.isArray(assets)) && <p>No assets available</p>)
 				}
 			</div>
 		</div>
