755c5b24ddac4e4e81bd4bc7956816a7b081d9a6
markofdao <dmitrydaopro@gmail.com>
2022-09-09 15:09:29 +0700

    update navbar bar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = ({ title, items, onButtonClick, buttonTitle }: Props) => {
     <>
       <nav className="navbar navbar-expand-lg bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">{title}</a>
+          <Link className="navbar-brand" to="/">{title}</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
