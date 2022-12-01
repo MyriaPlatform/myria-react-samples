80f093f338cc609efe80c8dd60c92799f37b8478
markofdao <dmitrydaopro@gmail.com>
2022-12-01 15:18:01 +0700

    update params

--- src/samples/erc20/deposit-erc20.ts
+++ src/samples/erc20/deposit-erc20.ts
@@ -1,18 +1,18 @@
 import { ConfirmationType, DepositERC20Params, DepositModule, MyriaClient, SendOptions } from "myria-core-sdk";
 import { convertEthToWei } from "../common/convertEthToWei";
 
-export async function depositErc20(client: MyriaClient, starkKey: string, publicKey: string, contractAddress: string, amount: string) {
+export async function depositErc20(client: MyriaClient, starkKey: string, account: string, contractAddress: string, amount: string) {
   const depositModule: DepositModule = new DepositModule(client);
 
   const params: DepositERC20Params = {
     starkKey: starkKey,
     contractAddress: contractAddress,
     amount: String(convertEthToWei(amount.toString())),
-    ethAddress: publicKey
+    ethAddress: account
   }
 
   const sendOptions: SendOptions = {
-    from: publicKey,
+    from: account,
     confirmationType: ConfirmationType.Confirmed,
   }
 
